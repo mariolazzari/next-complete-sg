1e6e076995ca98fe5ad624986af5e564d974a694
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-07 20:01:42 +0100

    Done chpater 7 lesson 66

--- discuss/src/actions/index.ts
+++ discuss/src/actions/index.ts
@@ -1,10 +1,4 @@
-"use server";
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn, signOut } from "./auth";
+export { createComment } from "./comments";
+export { createPost } from "./posts";
+export { createTopic } from "./topics";
