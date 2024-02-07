19a25e88b2d394b4a10399e374c08d2f480c0b78
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-07 22:32:50 +0100

    Done chpater 7 lesson 71

--- discuss/src/components/header.tsx
+++ discuss/src/components/header.tsx
@@ -13,47 +13,43 @@ import {
 } from "@nextui-org/react";
 import { auth } from "@/auth";
 import * as actions from "@/actions";
+import { ReactNode } from "react";
 
 export default async function Header() {
   const session = await auth();
 
-  let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ""} className="w-8 h-8" />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign In
-            </Button>
+  const authContent: ReactNode = session?.user ? (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Avatar src={session.user.image || ""} className="w-8 h-8" />
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="p-4">
+          <form action={actions.signOut}>
+            <Button type="submit">Sign Out</Button>
           </form>
-        </NavbarItem>
+        </div>
+      </PopoverContent>
+    </Popover>
+  ) : (
+    <>
+      <NavbarItem>
+        <form action={actions.signIn}>
+          <Button type="submit" color="secondary" variant="bordered">
+            Sign In
+          </Button>
+        </form>
+      </NavbarItem>
 
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
+      <NavbarItem>
+        <form action={actions.signIn}>
+          <Button type="submit" color="primary" variant="flat">
+            Sign Up
+          </Button>
+        </form>
+      </NavbarItem>
+    </>
+  );
 
   return (
     <Navbar className="shadow mb-6">
