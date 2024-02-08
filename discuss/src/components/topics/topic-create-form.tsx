ea3e96799e2c912e09c599f3efdbafc2b3b578e4
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-08 22:58:08 +0100

    Done chapter 7 lesson 74

--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,35 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+
 export const TopicCreateForm = () => {
-  return <div>TopicCreateForm</div>;
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form
+          action={actions.createTopic}
+          className="flex flex-col gap-4 p-4 w-80"
+        >
+          <h3 className="text-lg">Create a Topic</h3>
+          <Input label="Name" labelPlacement="outside" placeholder="name" />
+          <Textarea
+            label="Description"
+            labelPlacement="outside"
+            placeholder="Describe yout Topic"
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 };
