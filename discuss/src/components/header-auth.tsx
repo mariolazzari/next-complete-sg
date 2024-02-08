b64d009207cd56631135ddabf6d4fc67ce3371f7
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-08 19:06:17 +0100

    Done chpater 7 lesson 72

---
+++ discuss/src/components/header-auth.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { ReactNode } from "react";
+import {
+  Avatar,
+  Button,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+import * as actions from "@/actions";
+
+const HeaderAuth = () => {
+  const session = useSession();
+
+  if (session.status === "loading") {
+    return null;
+  }
+
+  const authContent: ReactNode = session.data?.user ? (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Avatar src={session.data.user.image || ""} className="w-8 h-8" />
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="p-4">
+          <form action={actions.signOut}>
+            <Button type="submit">Sign Out</Button>
+          </form>
+        </div>
+      </PopoverContent>
+    </Popover>
+  ) : (
+    <>
+      <NavbarItem>
+        <form action={actions.signIn}>
+          <Button type="submit" color="secondary" variant="bordered">
+            Sign In
+          </Button>
+        </form>
+      </NavbarItem>
+
+      <NavbarItem>
+        <form action={actions.signIn}>
+          <Button type="submit" color="primary" variant="flat">
+            Sign Up
+          </Button>
+        </form>
+      </NavbarItem>
+    </>
+  );
+
+  return authContent;
+};
+
+export default HeaderAuth;
