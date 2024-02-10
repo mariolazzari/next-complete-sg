a43a7f4aad28afc36d0df86470032706d1596c22
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-10 20:22:51 +0100

    Done chapter 7 lesson 88

--- discuss/src/actions/posts.ts
+++ discuss/src/actions/posts.ts
@@ -1,3 +1,49 @@
 "use server";
+import type { Post } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import paths from "@/paths";
 
-export async function createPost() {}
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+type CreatePostFormState = {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+};
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const res = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!res.success) {
+    return {
+      errors: res.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session?.user) {
+    return {
+      errors: {
+        _form: ["You must be logged in"],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
+}
--- discuss/src/app/topics/[slug]/page.tsx
+++ discuss/src/app/topics/[slug]/page.tsx
@@ -1,5 +1,25 @@
-const TopicShowPage = () => {
-  return <div>Topic Show</div>;
+import PostCreateForm from "@/components/posts/post-create-form";
+
+type TopicShowPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+const TopicShowPage = ({ params }: TopicShowPageProps) => {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">Topic Show: {slug}</h1>
+      </div>
+
+      <div>
+        <PostCreateForm />
+      </div>
+    </div>
+  );
 };
 
 export default TopicShowPage;
